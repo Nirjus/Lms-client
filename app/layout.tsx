8e3e607d7e7d00a7350c8f7a71923c40677b12b2
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-11-22 03:30:33 +0530

    final push

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }) {
  
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`} >
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Providers>
--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     images:{
         domains: ['res.cloudinary.com','lh3.googleusercontent.com','randomuser.me'],
     },
+    experimental:{
+        reactRoot: true,
+        suppressHydrationWarning: true,
+    }
 }
 
 module.exports = nextConfig
